075585a14d92cb11de4180a9b893306380721a45
DawJu <dwdjuszkiewicz@gmail.com>
2023-12-18 19:36:40 +0100

    Added a query and filtering for CharacterMovie

--- StarWars/GraphQL/model_nodes.py
+++ StarWars/GraphQL/model_nodes.py
@@ -1,7 +1,7 @@
 import graphene
 from graphene_django import DjangoObjectType
 
-from StarWars.models import Character, Movie
+from StarWars.models import Character, Movie, CharacterMovie
 
 
 class CharacterNode(DjangoObjectType):
@@ -15,4 +15,11 @@ class MovieNode(DjangoObjectType):
     class Meta:
         model = Movie
         filter_fields = ["name", "episode", "release_year"]
+        interfaces = (graphene.relay.Node,)
+
+
+class CharacterMovieNode(DjangoObjectType):
+    class Meta:
+        model = CharacterMovie
+        filter_fields = ["character", "movie"]
         interfaces = (graphene.relay.Node,)
--- StarWars/GraphQL/schema.py
+++ StarWars/GraphQL/schema.py
@@ -1,13 +1,28 @@
+import django_filters
 import graphene
 from graphene_django.filter.fields import DjangoFilterConnectionField
 
-from StarWars.GraphQL.model_nodes import CharacterNode, MovieNode
+from StarWars.GraphQL.model_nodes import CharacterNode, MovieNode, CharacterMovieNode
 import StarWars.GraphQL.mutations
+from StarWars.models import CharacterMovie
+
+
+class CharacterMovieFilter(django_filters.FilterSet):
+    character_name = django_filters.CharFilter(field_name="character__name", lookup_expr="icontains")
+    movie_name = django_filters.CharFilter(field_name="movie__name", lookup_expr="icontains")
+    movie_episode = django_filters.NumberFilter(field_name="movie__episode")
+
+    class Meta:
+        model = CharacterMovie
+        fields = ["character_name", "movie_name", "movie_episode"]
 
 
 class Query(graphene.ObjectType):
     characters = DjangoFilterConnectionField(CharacterNode)
     movies = DjangoFilterConnectionField(MovieNode)
+    character_movies = DjangoFilterConnectionField(
+        CharacterMovieNode, filterset_class=CharacterMovieFilter
+    )
 
 
 class Mutation(StarWars.GraphQL.mutations.Mutation, graphene.ObjectType):
