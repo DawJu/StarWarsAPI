aa22af0a44aabdc48de5255c7279a0f70c3abb8e
DawJu <dwdjuszkiewicz@gmail.com>
2023-12-18 16:07:05 +0100

    Added MovieNode and movies query

--- StarWars/GraphQL/model_nodes.py
+++ StarWars/GraphQL/model_nodes.py
@@ -1,11 +1,18 @@
 import graphene
 from graphene_django import DjangoObjectType
 
-from StarWars.models import Character
+from StarWars.models import Character, Movie
 
 
 class CharacterNode(DjangoObjectType):
     class Meta:
         model = Character
         filter_fields = ["name", "species", "gender", "age"]
+        interfaces = (graphene.relay.Node,)
+
+
+class MovieNode(DjangoObjectType):
+    class Meta:
+        model = Movie
+        filter_fields = ["name", "episode", "release_year"]
         interfaces = (graphene.relay.Node,)
--- StarWars/GraphQL/schema.py
+++ StarWars/GraphQL/schema.py
@@ -1,12 +1,13 @@
 import graphene
 from graphene_django.filter.fields import DjangoFilterConnectionField
 
-from StarWars.GraphQL.model_nodes import CharacterNode
+from StarWars.GraphQL.model_nodes import CharacterNode, MovieNode
 import StarWars.GraphQL.mutations
 
 
 class Query(graphene.ObjectType):
     characters = DjangoFilterConnectionField(CharacterNode)
+    movies = DjangoFilterConnectionField(MovieNode)
 
 
 class Mutation(StarWars.GraphQL.mutations.Mutation, graphene.ObjectType):
