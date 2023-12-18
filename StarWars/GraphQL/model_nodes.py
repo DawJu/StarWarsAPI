e8d0fa8dc2be3fb7d837d942d38317c9d0b9128d
DawJu <dwdjuszkiewicz@gmail.com>
2023-12-18 19:46:57 +0100

    Extended Character and Movie field filtering

--- StarWars/GraphQL/model_nodes.py
+++ StarWars/GraphQL/model_nodes.py
@@ -7,14 +7,23 @@ from StarWars.models import Character, Movie, CharacterMovie
 class CharacterNode(DjangoObjectType):
     class Meta:
         model = Character
-        filter_fields = ["name", "species", "gender", "age"]
+        filter_fields = {
+            "name": ["exact", "istartswith", "icontains"],
+            "species": ["exact", "istartswith", "icontains"],
+            "gender": ["exact", "istartswith", "icontains"],
+            "age": ["exact"]
+        }
         interfaces = (graphene.relay.Node,)
 
 
 class MovieNode(DjangoObjectType):
     class Meta:
         model = Movie
-        filter_fields = ["name", "episode", "release_year"]
+        filter_fields = {
+            "name": ["exact", "istartswith", "icontains"],
+            "episode": ["exact"],
+            "release_year": ["exact"]
+        }
         interfaces = (graphene.relay.Node,)
 
 
