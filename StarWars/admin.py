4cb02dae6cb2f101ab5c1ed90a4cd882d75de09b
DawJu <dwdjuszkiewicz@gmail.com>
2023-12-18 15:56:47 +0100

    Added Movie and CharacterMovie models

--- StarWars/admin.py
+++ StarWars/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 
-from .models import Character
+from StarWars.models import Character, Movie, CharacterMovie
 
 # Register your models here.
 
 admin.site.register(Character)
+admin.site.register(Movie)
+admin.site.register(CharacterMovie)
--- StarWars/models.py
+++ StarWars/models.py
@@ -24,3 +24,36 @@ class Character(models.Model):
 
     def __str__(self):
         return f"{self.name} ({self.character_id})"
+
+
+class Movie(models.Model):
+    movie_id = models.AutoField(primary_key=True)
+    name = models.TextField(max_length=100, verbose_name="Name")
+    episode = models.IntegerField(verbose_name="Episode")
+    release_year = models.IntegerField(verbose_name="Release Year")
+
+    class Meta:
+        verbose_name = "Movie"
+        verbose_name_plural = "Movies"
+
+    def __str__(self):
+        return f"Episode {self.episode} - {self.name} ({self.release_year})"
+
+
+class CharacterMovie(models.Model):
+    character_movie_id = models.AutoField(primary_key=True)
+    character = models.ForeignKey(Character,
+                                  on_delete=models.CASCADE,
+                                  related_name="characters",
+                                  related_query_name="character")
+    movie = models.ForeignKey(Movie,
+                              on_delete=models.CASCADE,
+                              related_name="movies",
+                              related_query_name="movie")
+
+    class Meta:
+        verbose_name = "Character + Movie"
+        verbose_name_plural = "Characters + Movies"
+
+    def __str__(self):
+        return f"{self.character.name} from {self.movie.name} ({self.character_movie_id})"
